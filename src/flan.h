6e9dc72816f4fea97db2678514813051cd66c3b6
leguu <33807061+Leguu@users.noreply.github.com>
2020-01-24 16:48:35 +0400

    Fully implemented subtraction and addition.

--- src/flan.h
+++ src/flan.h
@@ -12,13 +12,29 @@ struct flan {
 	digit *digits;
 };
 
+//
+// Declaration
+//
+
 struct flan *flan_init(int);
-void flan_free(struct flan *);
-bool flan_set(struct flan *, char *);
 struct flan *flan_init_set(int, char *);
+struct flan *flan_init_with(int, digit *);
+bool flan_set(struct flan *, char *);
+void flan_free(struct flan *);
+struct flan *flan_cpy(const struct flan *);
+
+void flan_add_digits(struct flan *, const struct flan *);
+void flan_add(struct flan *, const struct flan *);
+void flan_sub_digits(struct flan *, const struct flan *);
+void flan_sub(struct flan *, const struct flan *);
+
 int flan_cmp(const struct flan *, const struct flan *);
 int flan_num_digits(const struct flan *);
 char *flan_as_str(const struct flan *);
+
+//
+// Implementation
+//
 
 struct flan *flan_init(int len) {
 	struct flan *self = malloc(sizeof(struct flan));
@@ -26,6 +42,21 @@ struct flan *flan_init(int len) {
 	self->neg = false;
 	self->digits = calloc(len, sizeof(digit));
 	return self;
+}
+
+struct flan *flan_init_with(int len, digit *digits) {
+	struct flan *self = malloc(sizeof(struct flan));
+	self->len = len;
+	self->neg = false;
+	self->digits = digits;
+	return self;
+}
+
+struct flan *flan_cpy(const struct flan *self) {
+	digit *digits = calloc(self->len, sizeof(digit));
+	memcpy(digits, self->digits, self->len);
+	struct flan *result = flan_init_with(self->len, digits);
+	return result;
 }
 
 void flan_free(struct flan *self) {
@@ -61,6 +92,9 @@ struct flan *flan_init_set(int len, char *str) {
 	return self;
 }
 
+// Returns 0 if they're the same
+// Returns -1 if other > self
+// Returns 1 if self > other
 int flan_cmp(const struct flan *self, const struct flan *other) {
 	const int self_digits = flan_num_digits(self);
 	const int other_digits = flan_num_digits(other);
@@ -75,15 +109,7 @@ int flan_cmp(const struct flan *self, const struct flan *other) {
 	return 0;
 }
 
-void flan_add(struct flan *self, struct flan *other) {
-	if (other->neg)
-		; // flan_sub(self, other but negative);
-
-	if (self->neg)
-		; //Idk what to do here...
-
-	// If both self and other are positive...
-
+void flan_add_digits(struct flan *self, const struct flan *other) {
 	// First add all the digits together
 	for (int i = 0; i < self->len; i++) {
 		self->digits[i] += other->digits[i];
@@ -96,22 +122,62 @@ void flan_add(struct flan *self, struct flan *other) {
 			self->digits[i] = self->digits[i] % 10;
 		}
 	}
+
 }
 
-void flan_sub(struct flan *self, struct flan *other) {
+void flan_add(struct flan *self, const struct flan *other) {
+	if (other->neg && self->neg) {
+		flan_add_digits(self, other);
+	} else if (self->neg) {
+		struct flan *temp = flan_cpy(self);
+		temp->neg = false;
+
+		flan_free(self);
+		self = flan_cpy(other);
+
+		flan_sub(self, temp);
+
+		flan_free(temp);
+	} else {
+		flan_add_digits(self, other);
+	}
+}
+
+void flan_sub_digits(struct flan *self, const struct flan *other) {
 	for (int i = 0; i < self->len - 1; i++) {
 		if (self->digits[i] < other->digits[i]) {
-			for (int j = i + 1; i < self->len; j++) {
+			for (int j = i + 1; j < self->len; j++) {
 				if (j == i)
 					break;
 				if (self->digits[j] > 0) {
 					self->digits[j] -= 1;
 					self->digits[j - 1] += 10;
-					j -= 1;
+					j -= 2;
 				}
 			}
 		}
 		self->digits[i] -= other->digits[i];
+	}
+}
+
+void flan_sub(struct flan *self, const struct flan *other) {
+	if (self->neg && other->neg) {
+		flan_sub_digits(self, other);
+	} else if (other->neg || self->neg) {
+		flan_add_digits(self, other); 
+	} else {
+		// If both self and other are positive...
+		// and if self is less than other
+		if (flan_cmp(self, other) < 0) {
+			struct flan *temp = flan_cpy(self);
+			flan_free(self);
+			self = flan_cpy(other);
+			self->neg = true;
+			flan_sub_digits(self, temp);
+			flan_free(temp);
+		} else {
+			flan_sub_digits(self, other);
+		}
 	}
 }
 
--- src/main.c
+++ src/main.c
@@ -2,16 +2,17 @@
 #include "flan.h"
 
 int main() {
-	struct flan *flan1 = flan_init_set(10, "25");
+	struct flan *flan1 = flan_init_set(10, "20");
 	struct flan *flan2 = flan_init_set(10, "21");
 
-	printf("actual: %d\n", flan_num_digits(flan1));
-
-	printf("flan: %s\n", flan_as_str(flan1));
+	printf("4: FLAN1: %s, FLAN2: %s\n", flan_as_str(flan1), flan_as_str(flan2));
 
 	flan_sub(flan1, flan2);
 
-	printf("%d\n", flan_cmp(flan1, flan2));
+	printf("5: FLAN1: %s, FLAN2: %s\n", flan_as_str(flan1), flan_as_str(flan2));
 
-	printf("flan: %s\n", flan_as_str(flan1));
+	flan_set(flan2, "500");
+	flan_add(flan1, flan2);
+
+	printf("6: FLAN1: %s, FLAN2: %s\n", flan_as_str(flan1), flan_as_str(flan2));
 }
